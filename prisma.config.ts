26c3069cd4396b818331fa1dc5ee5758ce9951d7
Abdullah Shakir <bbd29318@gmail.com>
2025-12-01 10:53:34 +0300

    final version completed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig } from '@prisma/config';
-import { createClient } from '@libsql/client';
-import { PrismaLibSQL } from '@prisma/adapter-libsql';
 
 export default defineConfig({
-	adapter: () => {
-		const client = createClient({
-			url: process.env.DATABASE_URL!,
-			authToken: process.env.TURSO_AUTH_TOKEN
-		});
-
-		return new PrismaLibSQL(client);
-	}
+	schema: './prisma/schema.prisma'
 });
